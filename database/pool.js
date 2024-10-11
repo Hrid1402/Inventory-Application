34494a5803346846ac9d6a2a9e83fe30329c75ff
Hrid1402 <Hrid1402@gmail.com>
2024-10-11 12:08:27 -0500

    feat: url sql connetcion

--- database/pool.js
+++ database/pool.js
@@ -5,9 +5,8 @@ const { Pool } = require("pg");
 
 
 module.exports = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT
+  connectionString: process.env.URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
