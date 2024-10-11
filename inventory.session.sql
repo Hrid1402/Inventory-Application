5640c02801724ca355ed209cc5abbc9cd4feff06
Hrid1402 <Hrid1402@gmail.com>
2024-10-10 21:50:16 -0500

    feat: clean finished

--- inventory.session.sql
+++
@@ -1,4 +0,0 @@
-
---INSERT INTO items (name, quantity, description, price, category)
---VALUES ('Tablet', 2, 'Pink cool tablet', 90.5, 'Electronics');
-SELECT * FROM items WHERE category = 'Electronics';
